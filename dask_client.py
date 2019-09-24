d7e171a56956311311c0d1e247e40987e9a83244
yatharthranjan <yatharthranjan89@gmail.com>
2019-09-24 15:15:13 +0100

    Add support for relative location based clustering

--- dask_client.py
+++ dask_client.py
@@ -2,8 +2,8 @@
 from dask.distributed import Client, progress
 
 def run_client():
-    client = Client('10.131.135.81:8786', n_workers=2, threads_per_worker=2, memory_limit='4GB')
-    client
+    client = Client('10.200.107.9:8786', n_workers=3, threads_per_worker=4, memory_limit='10GB')
+    return client
 
 if __name__ == '__main__':
     run_client()
